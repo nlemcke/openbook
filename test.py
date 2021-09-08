13a38c89a28eebd21bc031e7c1683386c5732f5d
nlemcke <nathanjlemcke@gmail.com>
2021-09-08 14:55:22 -0700

    type knowledge

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 """
 Put my tests here:
 
-    >>>
+  >>> type(thing1)
+  <class 'list'>
+  >>> type(thing2)
+  <class 'tuple'>
+  >>> type(thing3)
+  <class 'str'>
 
 """
 # Put your solutions here:
 
+thing1 = [1, 2, 3]
+thing2 = (1, 2, 3)
+thing3 = 'bananas'
 
 if __name__ == "__main__":
     import doctest
-    doctest.test(mod)
+    doctest.testmod()
