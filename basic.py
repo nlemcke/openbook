d774fd3927aa5ab02d0f81ad9908e1db0b883eda
nlemcke <nathanjlemcke@gmail.com>
2021-09-10 13:10:14 -0700

    making progress

--- basic.py
+++ basic.py
@@ -1,3 +1,4 @@
 import random
-num = rand.randrange(1, 100)
+num = random.randrange(1, 100)
+guess = int(input("Guess the number I'm thinking of between 1 and 100: ")
 print(num)
--- play.py
+++ play.py
@@ -1,17 +1,17 @@
 import random
 
-number = random.randrange(1, 1000)
+number = random.randrange(1, 100)
 guesses = 0
-guess = int(input("Guess my number between 1 and 1000: "))
+guess = int(input("Guess my number between 1 and 100: "))
 
 while guess != number:
     guesses += 1
     print("Sorry, wrong answer.")
     if guess > number:
         print("My number is closer to 0")
-        guess = int(input("Guess again: ")
+        guess = int(input("Guess again: "))
     else:
-        print("My number is closer to 1000")
-        guess = int(input("Guess again: ")
+        print("My number is closer to 100")
+        guess = int(input("Guess again: "))
 
-print("Good job", number, "is my number")
+print("Good job", number, "is my number!")
