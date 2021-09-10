6d0a01cb4a1d61c217f19e35081fdb27c4ca29b1
nlemcke <nathanjlemcke@gmail.com>
2021-09-10 12:46:46 -0700

    experimenting2

--- basic.py
+++ basic.py
@@ -1,6 +1,3 @@
-num = int(input("What's your number?"))
-
-if num < 10:
-    print("I like it lower than that")
-else:
-    print("I like it a little higher than that")
+import random
+num = rand.randrange(1, 100)
+print(num)
