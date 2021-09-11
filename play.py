c9bb535f5cdac9afe3a9e89d1ce6ed0de3a54d9f
nlemcke <nathanjlemcke@gmail.com>
2021-09-10 17:08:56 -0700

    topping it off

--- play.py
+++ play.py
@@ -1,8 +1,10 @@
-prefixes = "JKLMNOPQ"
-suffix = "ack"
+n = int(input("Tell me a number: "))
 
-for letter in prefixes:
-    if letter != 'O' and letter !='Q':
-        print(letter + suffix)
-    else:
-        print(letter + "u" + suffix)
+count = 0
+while n > 0:
+    digit = n % 10
+    if digit == 0 or digit == 5:
+        count += 1
+    n = n // 10
+
+print(count)
