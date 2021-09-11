bc108ded41d7844cd69ceb7fbdb0851982156104
nlemcke <nathanjlemcke@gmail.com>
2021-09-11 13:55:34 -0700

    first commit of the day

--- basic.py
+++ basic.py
@@ -1,4 +1,1 @@
-import random
-num = random.randrange(1, 100)
-guess = int(input("Guess the number I'm thinking of between 1 and 100: ")
-print(num)
+print('Hello world!')
--- play.py
+++ play.py
@@ -1,12 +1,1 @@
-print("NUM\tDIV BY 2 OR 3")
-print("---\t-------------")
-for num in range(2, 21):
-    if num % 2 == 0 and num % 3 == 0:
-        print(num, "\tboth"
-    elif num % 2 == 0:
-        print(num, "\tdiv by 2")
-    elif num % 3 == 0:
-        print(num, "\tdiv by 3")
-    else:
-        print(num, "\tneither")
-
+print('Hello World')
