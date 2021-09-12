9fecaa260b1d85b50d23064cd46298d01d89c280
nlemcke <nathanjlemcke@gmail.com>
2021-09-12 12:05:46 -0700

    update

--- play.py
+++ play.py
@@ -1,12 +1,1 @@
-def recursive_max(nestedlist):
-    
-    largest = nestedlist[0]
-    while type(largest) == type([]):
-        largest = largest[0]
-
-    for i in nestedlist:
-        if type(i) == type([]):
-            max_elem = recursive_max(i)
-
-        else:
-            largest = 
+ls
